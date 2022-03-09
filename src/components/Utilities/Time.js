4af37ba10eebab72919962ba02c0988654845918
Shalom Mathew <98167046+shalomwayapaychat@users.noreply.github.com>
2022-03-09 23:47:46 +0100

    updated

--- src/components/Utilities/Time.js
+++ src/components/Utilities/Time.js
@@ -75,15 +75,10 @@ export const unformat = (amount, locale) => {
 }
 export const monthNames = [
   'Jan',
-  'Feb',
   'Mar',
-  'Apr',
   'May',
-  'Jun',
   'Jul',
-  'Aug',
   'Sept',
-  'Oct',
   'Nov',
-  'Dec',
+  'Jan',
 ]
