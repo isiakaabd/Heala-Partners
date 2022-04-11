d32dfcf72aabcf86d9215e4d8cf7ce30c08bde8d
Shalom Mathew <98167046+shalomwayapaychat@users.noreply.github.com>
2022-04-11 12:19:54 +0100

    fix forms

--- src/components/pages/PendingOrder.js
+++ src/components/pages/PendingOrder.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import { dateMoment } from 'components/Utilities/Time'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { dateMoment } from "components/Utilities/Time";
 import {
   Grid,
   Button,
@@ -11,138 +11,145 @@ import {
   FormLabel,
   Checkbox,
   TableCell,
-} from '@mui/material'
-import { Modals, Search, FilterList, Loader } from 'components/Utilities'
-import FormSelect from 'components/Utilities/FormSelect'
-import EnhancedTable from 'components/layouts/EnhancedTable'
-import { makeStyles } from '@mui/styles'
-import { useTheme } from '@mui/material/styles'
-import { patientsHeadCells } from 'components/Utilities/tableHeaders'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import displayPhoto from 'assets/images/avatar.svg'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useActions } from 'components/hooks/useActions'
-import { handleSelectedRows } from 'helpers/selectedRows'
-import { isSelected } from 'helpers/isSelected'
-import useFormInput from 'components/hooks/useFormInput'
-import { useQuery } from '@apollo/client'
-import { getDrugOrders } from 'components/graphQL/useQuery'
-import { NoData, EmptyTable } from 'components/layouts' //
+} from "@mui/material";
+import { Modals, Search, FilterList, Loader } from "components/Utilities";
+import FormSelect from "components/Utilities/FormSelect";
+import EnhancedTable from "components/layouts/EnhancedTable";
+import { makeStyles } from "@mui/styles";
+import { useTheme } from "@mui/material/styles";
+import { patientsHeadCells } from "components/Utilities/tableHeaders";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import displayPhoto from "assets/images/avatar.svg";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useActions } from "components/hooks/useActions";
+import { handleSelectedRows } from "helpers/selectedRows";
+import { isSelected } from "helpers/isSelected";
+import useFormInput from "components/hooks/useFormInput";
+import { useQuery } from "@apollo/client";
+import { getDrugOrders } from "components/graphQL/useQuery";
+import { NoData, EmptyTable } from "components/layouts"; //
+import prettyMoney from "pretty-money";
 
-const referralOptions = ['Hello', 'World', 'Goodbye', 'World']
+const referralOptions = ["Hello", "World", "Goodbye", "World"];
 
-const plans = ['Plan 1', 'Plan 2', 'Plan 3', 'Plan 4']
-const genderType = ['Male', 'Female', 'Prefer not to say']
-const statusType = ['Active', 'Blocked']
+const plans = ["Plan 1", "Plan 2", "Plan 3", "Plan 4"];
+const genderType = ["Male", "Female", "Prefer not to say"];
+const statusType = ["Active", "Blocked"];
 
 const useStyles = makeStyles((theme) => ({
   searchGrid: {
-    '&.MuiGrid-root': {
+    "&.MuiGrid-root": {
       flex: 1,
-      marginRight: '5rem',
+      marginRight: "5rem",
     },
   },
   button: {
-    '&.MuiButton-root': {
-      background: '#fff',
+    "&.MuiButton-root": {
+      background: "#fff",
       color: theme.palette.common.grey,
-      textTransform: 'none',
-      borderRadius: '2rem',
-      display: 'flex',
-      alignItems: 'center',
-      padding: '1rem',
-      maxWidth: '10rem',
+      textTransform: "none",
+      borderRadius: "2rem",
+      display: "flex",
+      alignItems: "center",
+      padding: "1rem",
+      maxWidth: "10rem",
 
-      '&:hover': {
-        background: '#fcfcfc',
+      "&:hover": {
+        background: "#fcfcfc",
       },
 
-      '&:active': {
-        background: '#fafafa',
+      "&:active": {
+        background: "#fafafa",
       },
 
-      '& .MuiButton-endIcon>*:nth-of-type(1)': {
-        fontSize: '1.2rem',
+      "& .MuiButton-endIcon>*:nth-of-type(1)": {
+        fontSize: "1.2rem",
       },
 
-      '& .MuiButton-endIcon': {
-        marginLeft: '.3rem',
-        marginTop: '-.2rem',
+      "& .MuiButton-endIcon": {
+        marginLeft: ".3rem",
+        marginTop: "-.2rem",
       },
     },
   },
 
   tableCell: {
-    '&.MuiTableCell-root': {
-      fontSize: '1.25rem',
-      textAlign: 'left',
+    "&.MuiTableCell-root": {
+      fontSize: "1.25rem",
+      textAlign: "left",
     },
   },
 
   badge: {
-    '&.MuiChip-root': {
-      fontSize: '1.25rem !important',
-      height: '2.7rem',
+    "&.MuiChip-root": {
+      fontSize: "1.25rem !important",
+      height: "2.7rem",
 
-      borderRadius: '1.3rem',
+      borderRadius: "1.3rem",
     },
   },
   chip: {
-    '&.MuiChip-root': {
-      fontSize: '1.25rem',
-      height: '3rem',
-      borderRadius: '1.3rem',
+    "&.MuiChip-root": {
+      fontSize: "1.25rem",
+      height: "3rem",
+      borderRadius: "1.3rem",
       background: theme.palette.common.white,
       color: theme.palette.common.grey,
-      '& .MuiChip-deleteIcon': {
-        color: 'inherit',
-        fontSize: 'inherit',
+      "& .MuiChip-deleteIcon": {
+        color: "inherit",
+        fontSize: "inherit",
       },
     },
   },
   searchFilterBtn: {
-    '&.MuiButton-root': {
+    "&.MuiButton-root": {
       ...theme.typography.btn,
       background: theme.palette.common.black,
-      width: '100%',
+      width: "100%",
     },
   },
-}))
+}));
 
 const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
-  const classes = useStyles()
-  const theme = useTheme()
-  const [state, setState] = useState([])
-  const orderState = 'pending'
+  const classes = useStyles();
+  const theme = useTheme();
+  const [state, setState] = useState([]);
+  const orderState = "pending";
   const { data, loading, error } = useQuery(getDrugOrders, {
     variables: { status: orderState },
-  })
+  });
   useEffect(() => {
-    if (data) return setState(data?.getDrugOrders.data)
-  }, [data])
+    if (data) return setState(data?.getDrugOrders.data);
+  }, [data]);
 
   const [inputValue, handleInputValue] = useFormInput({
-    date: '',
-    plan: '',
-    gender: '',
-    status: '',
-  })
+    date: "",
+    plan: "",
+    gender: "",
+    status: "",
+  });
 
-  const { date, plan, gender, status } = inputValue
+  const { date, plan, gender, status } = inputValue;
 
   const { rowsPerPage, selectedRows, page } = useSelector(
-    (state) => state.tables,
-  )
-  const { setSelectedRows } = useActions()
+    (state) => state.tables
+  );
+  const { setSelectedRows } = useActions();
 
-  const [searchPatient, setSearchPatient] = useState('')
-  const [isOpen, setIsOpen] = useState(false)
+  const [searchPatient, setSearchPatient] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleDialogOpen = () => setIsOpen(true)
-  const handleDialogClose = () => setIsOpen(false)
-  if (loading) return <Loader />
-  if (error) return <NoData error={error} />
+  const handleDialogOpen = () => setIsOpen(true);
+  const handleDialogClose = () => setIsOpen(false);
+  if (loading) return <Loader />;
+  if (error) return <NoData error={error} />;
+  const prettyDollarConfig = {
+    currency: "₦",
+    position: "before",
+    spaced: false,
+    thousandsDelimiter: ",",
+  };
   return (
     <>
       <Grid
@@ -152,7 +159,7 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
         flexWrap="nowrap"
         height="100%"
       >
-        <Grid item container style={{ paddingBottom: '5rem' }}>
+        <Grid item container style={{ paddingBottom: "5rem" }}>
           <Grid item className={classes.searchGrid}>
             <Search
               value={searchPatient}
@@ -190,10 +197,14 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
                       affliation,
                       patientData,
                       doctorData,
-                    } = row
-                    const isItemSelected = isSelected(_id, selectedRows)
+                      prescriptions,
+                    } = row;
+                    const isItemSelected = isSelected(_id, selectedRows);
 
-                    const labelId = `enhanced-table-checkbox-${index}`
+                    const labelId = `enhanced-table-checkbox-${index}`;
+                    const x = prescriptions.map((i) => {
+                      return i.drugPrice;
+                    });
 
                     return (
                       <TableRow
@@ -210,13 +221,13 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
                               handleSelectedRows(
                                 _id,
                                 selectedRows,
-                                setSelectedRows,
+                                setSelectedRows
                               )
                             }
                             color="primary"
                             checked={isItemSelected}
                             inputProps={{
-                              'aria-labelledby': labelId,
+                              "aria-labelledby": labelId,
                             }}
                           />
                         </TableCell>
@@ -236,49 +247,57 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
                         <TableCell align="left" className={classes.tableCell}>
                           <div
                             style={{
-                              height: '100%',
-                              display: 'flex',
-                              alignItems: 'center',
+                              height: "100%",
+                              display: "flex",
+                              alignItems: "center",
                             }}
                           >
-                            <span style={{ marginRight: '1rem' }}>
+                            <span style={{ marginRight: "1rem" }}>
                               <Avatar
                                 alt={`Display Photo of ${doctorData?.firstName}`}
                                 src={doctorData?.picture || displayPhoto}
                                 sx={{ width: 24, height: 24 }}
                               />
                             </span>
-                            <span style={{ fontSize: '1.25rem' }}>
+                            <span style={{ fontSize: "1.25rem" }}>
                               {doctorData
                                 ? `${doctorData?.firstName} ${doctorData?.lastName}`
-                                : 'No Value'}
+                                : "No Value"}
                             </span>
                           </div>
                         </TableCell>
                         <TableCell align="left" className={classes.tableCell}>
                           <div
                             style={{
-                              height: '100%',
-                              display: 'flex',
-                              alignItems: 'center',
+                              height: "100%",
+                              display: "flex",
+                              alignItems: "center",
                             }}
                           >
-                            <span style={{ marginRight: '1rem' }}>
+                            <span style={{ marginRight: "1rem" }}>
                               <Avatar
                                 alt={`Display Photo of ${patientData?.firstName}`}
                                 src={patientData?.image || displayPhoto}
                                 sx={{ width: 24, height: 24 }}
                               />
                             </span>
-                            <span style={{ fontSize: '1.25rem' }}>
+                            <span style={{ fontSize: "1.25rem" }}>
                               {patientData
                                 ? `${patientData?.firstName} ${patientData?.lastName}`
-                                : 'No Value'}
+                                : "No Value"}
                             </span>
                           </div>
                         </TableCell>
                         <TableCell align="left" className={classes.tableCell}>
-                          {affliation ? affliation : 'No Value'}
+                          {prettyMoney(
+                            prettyDollarConfig,
+                            x.reduce(function (accumulator, currentValue) {
+                              return accumulator + currentValue;
+                            }, 0)
+                          )}
+                        </TableCell>
+                        <TableCell align="left" className={classes.tableCell}>
+                          {x.length}
                         </TableCell>
 
                         <TableCell>
@@ -286,19 +305,19 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
                             label="view request"
                             variant="outlined"
                             onClick={() => {
-                              setSelectedSubMenu(2)
-                              setSelectedPatientMenu(0)
+                              setSelectedSubMenu(2);
+                              setSelectedPatientMenu(0);
                             }}
                             component={Link}
                             to={`pending-order/${_id}/order`}
                             className={classes.chip}
                             type="pending"
                             deleteIcon={<ArrowForwardIosIcon />}
-                            onDelete={() => console.log(' ')}
+                            onDelete={() => console.log(" ")}
                           />
                         </TableCell>
                       </TableRow>
-                    )
+                    );
                   })}
             </EnhancedTable>
           </Grid>
@@ -360,7 +379,7 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item style={{ marginBottom: '18rem', marginTop: '3rem' }}>
+          <Grid item style={{ marginBottom: "18rem", marginTop: "3rem" }}>
             <Grid container spacing={2}>
               <Grid item md>
                 <Grid container direction="column">
@@ -370,7 +389,7 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
                     </FormLabel>
                   </Grid>
                   <Grid item>
-                    <FormControl fullWidth style={{ height: '3rem' }}>
+                    <FormControl fullWidth style={{ height: "3rem" }}>
                       <FormSelect
                         name="gender"
                         options={genderType}
@@ -390,7 +409,7 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
                     </FormLabel>
                   </Grid>
                   <Grid item>
-                    <FormControl fullWidth style={{ height: '3rem' }}>
+                    <FormControl fullWidth style={{ height: "3rem" }}>
                       <FormSelect
                         name="status"
                         options={statusType}
@@ -417,12 +436,12 @@ const PendingOrder = ({ setSelectedSubMenu, setSelectedPatientMenu }) => {
         </Grid>
       </Modals>
     </>
-  )
-}
+  );
+};
 
 PendingOrder.propTypes = {
   setSelectedSubMenu: PropTypes.func.isRequired,
   setSelectedPatientMenu: PropTypes.func.isRequired,
-}
+};
 
-export default PendingOrder
+export default PendingOrder;
