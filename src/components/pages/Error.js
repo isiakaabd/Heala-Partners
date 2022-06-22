122537aa9bccfc9525886702ba4d53a6d82c3ffb
Remlad <isiakaabd@gmail.com>
2022-06-22 17:48:16 +0100

    added

---
+++ src/components/pages/Error.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Typography, Grid } from "@mui/material";
+import logo from "assets/images/logo.svg";
+const Error = () => {
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      flexDirection={"column"}
+      sx={{ height: "100vh" }}
+      padding={{ sm: 6, xs: 3 }}
+    >
+      <Grid item>
+        <img
+          src={logo}
+          alt="logo"
+          sx={{
+            marginBottom: "auto",
+            display: "inline-block",
+            height: "5rem",
+          }}
+        />
+      </Grid>
+      <Grid
+        item
+        container
+        flex={1}
+        justifyContent={"center"}
+        alignItems="center"
+        flexDirection={"column"}
+      >
+        <Typography variant="h1" color="#3e5ea9" gutterBottom>
+          Oops..!!!
+        </Typography>
+        <Typography variant="h4">Something Went Wrong...</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Error;
