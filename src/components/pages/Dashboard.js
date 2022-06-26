f07d687e5c92d8ef3f08dd4f94283acb1e575634
Remlad <isiakaabd@gmail.com>
2022-06-26 23:57:22 +0100

    added babel config file

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid } from "@mui/material";
-import DashboardCharts from "components/layouts/DashboardChart";
+import { DashboardCharts } from "components/layouts";
 
 const Dashboard = () => {
   return (
